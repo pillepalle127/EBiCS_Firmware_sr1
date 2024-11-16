9f858f7d93c4655e5a402f1a6e0a1a5020b539a1
pillepalle127 <stefan.ruehle@gmail.com>
2024-11-16 12:45:27 +0100

    Update config.h

--- Inc/config.h
+++ Inc/config.h
@@ -75,7 +75,7 @@
 //--------------------------------------------------------------------
 //Speed settings
 #define WHEEL_CIRCUMFERENCE 2000 //507er Reifen
-#define GEAR_RATIO 50 //11 for BionX IGH3 // wird nicht verwendet
+#define GEAR_RATIO 35 //11 for BionX IGH3 // wird nicht verwendet
 #define SPEEDLIMIT 26
 #define PULSES_PER_REVOLUTION 1
 #define SPEEDSOURCE INTERNAL
