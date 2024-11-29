a19aef19e42d66030da7fb2a56eb0b49e176202c
pillepalle127 <stefan.ruehle@gmail.com>
2024-11-29 09:24:22 +0100

    Update main.c

--- Src/main.c
+++ Src/main.c
@@ -184,8 +184,9 @@ q31_t q31_u_q_temp=0;
 int16_t i16_sinus=0;
 int16_t i16_cosinus=0;
 char buffer[100];
-char char_dyn_adc_state_old=1;
-const uint8_t assist_factor[10]={0, 91, 132, 173, 214, 255, 255, 255, 255, 255};
+char char_dyn_adc_state_old=1;const uint8_t assist_factor[10]={0, 67, 114, 161, 208, 255, 255, 255, 255, 255};
+//const uint8_t assist_factor[10]={0, 75, 120, 165, 210, 255, 255, 255, 255, 255};
+//const uint8_t assist_factor[10]={0, 111, 147, 183, 219, 255, 255, 255, 255, 255};
 const uint8_t assist_profile[2][6]= {	{0,10,20,30,45,48},
 		{64,64,128,200,255,0}};
 
